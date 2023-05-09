c214519f0e5e7d7bfc6e1f19130845eb39622a2e
Diogo Sales <diogodobu@gmail.com>
2023-05-09 19:27:55 -0300

    Criado componente Rodape

---
+++ components/Rodape.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Rodape = () => {
+  return (
+    <div
+      style={{ width: "100%", backgroundColor: "green" }}
+      className="bg-secondary position-fixed bottom-0 text-white text-center py-1"
+    >
+      <p>Todos os direitos reservados</p>
+    </div>
+  );
+};
+
+export default Rodape;
